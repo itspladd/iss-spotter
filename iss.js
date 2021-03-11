4c2cce7a6146addf7ec36690659ff173be9f8072
Paul Ladd <vlendle@gmail.com>
2021-03-11 21:41:29 +0000

    Printing entire results of geolocation req

--- iss.js
+++ iss.js
@@ -18,7 +18,19 @@ const fetchIP = (callback) => {
 };
 
 const fetchCoordsByIP = (ip, callback) => {
-  //TODO: Fill out
+  request.get(`https://freegeoip.app/json/${ip}`, (err, response, body) => {
+    // If error, print it out
+    if (err) {
+      callback(err, null);
+    } else if (response.statusCode !== 200) {
+      // If there was a non-success response code, log it and throw an error
+      const msg = `Status code ${response.statusCode} when fetching coordinates. Response was: ${body}`;
+      callback(Error(msg), null);
+    } else {
+      //Otherwise we should have coordinates! We only want lat and long.
+      callback(null, JSON.parse(body));
+    }
+  });
 };
 
 module.exports = { 
